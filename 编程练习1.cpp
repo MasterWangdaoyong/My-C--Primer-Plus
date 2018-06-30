42bb2619ff651e02d43a6f80e8851448c62957a8
JianpingWang <172099994@qq.com>
2018-06-30 23:57:10 +0800

    Update 编程练习1.cpp

--- 编程练习1.cpp
+++ 编程练习1.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main()
 {
-    cout << "我的名字叫王道勇。";
-    cout << "我的地址是：湖南省怀化市溆浦县九溪江乡九组。";
+    cout << "我的名字叫道勇。";
+    cout << "我的地址是：~~~~~~~~~~~~~fixed。";
     cin.get();
     cin.get();
     return 0;
